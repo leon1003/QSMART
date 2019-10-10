cf9680a5db56e728ff6d0e875434dbb280fc28a1
leon1003 <lh10630@uga.edu>
2019-10-10 14:11:40 -0400

    Add files via upload

---
+++ Model/Genomics/RF/autonomic_ganglia.sh
@@ -0,0 +1,4 @@
+module load Java/1.8.0_144
+java -classpath /Tool/WEKA/weka.jar weka.filters.unsupervised.attribute.Remove -R 2-3 -i /Data/Training/Genomics/autonomic_ganglia.csv -o /Data/Model/Genomics_RF/autonomic_ganglia_tmp.arff
+java -classpath /Tool/WEKA/weka.jar weka.classifiers.trees.RandomForest -P 100 -I 200 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -c 1 -x 10 -t /Data/Model/Genomics_RF/autonomic_ganglia_tmp.arff > /Data/Model/Genomics_RF/autonomic_ganglia.txt
+rm /Data/Model/Genomics_RF/autonomic_ganglia_tmp.arff
---
+++ Model/Genomics/RF/lymphoid.sh
@@ -0,0 +1,4 @@
+module load Java/1.8.0_144
+java -classpath /Tool/WEKA/weka.jar weka.filters.unsupervised.attribute.Remove -R 2-3 -i /Data/Training/Genomics/lymphoid.csv -o /Data/Model/Genomics_RF/lymphoid_tmp.arff
+java -classpath /Tool/WEKA/weka.jar weka.classifiers.trees.RandomForest -P 100 -I 300 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -c 1 -x 10 -t /Data/Model/Genomics_RF/lymphoid_tmp.arff > /Data/Model/Genomics_RF/lymphoid.txt
+rm /Data/Model/Genomics_RF/lymphoid_tmp.arff
