882b2fdf1355bb673404de704959cfbefa81b706
leon1003 <lh10630@uga.edu>
2019-10-10 15:13:15 -0400

    Add files via upload

---
+++ Model/Random/SVM/upper_aerodigestive_tract.sh
@@ -0,0 +1,4 @@
+module load Java/1.8.0_144
+java -classpath /Tool/WEKA/weka.jar weka.filters.unsupervised.attribute.Remove -R 2-3 -i /Data/Training/Random/upper_aerodigestive_tract.csv -o /Data/Model/Random_SVM/upper_aerodigestive_tract_tmp.arff
+java -classpath /Tool/WEKA/weka.jar weka.classifiers.functions.SMOreg -C 1.0 -N 0 -I "weka.classifiers.functions.supportVector.RegSMOImproved -T 0.001 -V -P 1.0E-12 -L 0.001 -W 1" -K "weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007" -c 1 -x 10 -t /Data/Model/Random_SVM/upper_aerodigestive_tract_tmp.arff > /Data/Model/Random_SVM/upper_aerodigestive_tract.txt
+rm /Data/Model/Random_SVM/upper_aerodigestive_tract_tmp.arff
