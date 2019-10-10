f1174a8ae65238f1ee0100c6029cc7dfe3dbe85b
leon1003 <lh10630@uga.edu>
2019-10-10 15:25:12 -0400

    Add files via upload

---
+++ Model/Random10X/Lasso/ovary.R
@@ -0,0 +1,8 @@
+library(glmnet)
+mydata = read.table('/Data/Training/Random10X/ovary.csv',head=T,sep=",")
+x = as.matrix(mydata[,4:ncol(mydata)])
+y = as.matrix(mydata[,1])
+lasso = cv.glmnet(x,y,nfolds=10,type.measure="mse",alpha=1,family="gaussian")
+sink('/Data/Model/Random10X_Lasso/ovary.txt',append=TRUE)
+print(lasso)
+sink()
