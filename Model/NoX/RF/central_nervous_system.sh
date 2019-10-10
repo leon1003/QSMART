4c95a384d1f9e11637d57ae1e9857a80adddd446
leon1003 <lh10630@uga.edu>
2019-10-10 14:56:58 -0400

    Add files via upload

---
+++ Model/NoX/RF/central_nervous_system.sh
@@ -0,0 +1,4 @@
+module load Java/1.8.0_144
+java -classpath /Tool/WEKA/weka.jar weka.filters.unsupervised.attribute.Remove -R 2-3 -i /Data/Training/NoX/central_nervous_system.csv -o /Data/Model/NoX_RF/central_nervous_system_tmp.arff
+java -classpath /Tool/WEKA/weka.jar weka.classifiers.trees.RandomForest -P 100 -I 300 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -c 1 -x 10 -t /Data/Model/NoX_RF/central_nervous_system_tmp.arff > /Data/Model/NoX_RF/central_nervous_system.txt
+rm /Data/Model/NoX_RF/central_nervous_system_tmp.arff
