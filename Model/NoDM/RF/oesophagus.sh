f45c24cd212fc5c8e546ec9301cf36466054317b
Liang-Chin Huang <lh10630@uga.edu>
2019-12-18 15:37:30 -0500

    Add files via upload

---
+++ Model/NoDM/RF/oesophagus.sh
@@ -0,0 +1,4 @@
+module load Java/1.8.0_144
+java -classpath /Tool/WEKA/weka.jar weka.filters.unsupervised.attribute.Remove -R 2-3 -i /Data/Training/NoDM/oesophagus.csv -o /Data/Model/NoDM/oesophagus_tmp.arff
+java -classpath /Tool/WEKA/weka.jar weka.classifiers.trees.RandomForest -P 100 -I 200 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -c 1 -x 10 -t /Data/Model/NoDM/oesophagus_tmp.arff > /Data/Model/NoDM/oesophagus.txt
+rm /Data/Model/NoDM/oesophagus_tmp.arff
